4c07bb0648ef6df5cd6558206397dd672b8c2896
Nguyễn Duy Đạt <32428939+duydat1998@users.noreply.github.com>
2019-03-22 22:01:31 +0700

    Update MyFirebaseMessagingService.java

--- app/src/main/java/swd/affiliatemarketing/hotpromotion/service/MyFirebaseMessagingService.java
+++ app/src/main/java/swd/affiliatemarketing/hotpromotion/service/MyFirebaseMessagingService.java
@@ -1,11 +1,13 @@
 package swd.affiliatemarketing.hotpromotion.service;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -16,6 +18,8 @@ import swd.affiliatemarketing.hotpromotion.MainActivity;
 import swd.affiliatemarketing.hotpromotion.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private NotificationManager notifManager;
+
     public MyFirebaseMessagingService() {
     }
 
@@ -34,25 +38,50 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void showPromotionCodeTrackingNotification(String promotionCode, String time) {
+        String date = time.substring(0,11);
+        String hour = time.substring(12,20);
+        time = hour + " " + date;
         String message = "Promotion code: " + promotionCode+" is used\n"+"At " + time;
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
+        if (notifManager == null) {
+            notifManager = (NotificationManager)getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        final int NOTIFY_ID = 0; // ID of notification
+        String id = getApplicationContext().getString(R.string.default_notification_channel_id); // default_channel_id
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setContentTitle("Promotion code used")
-                .setSmallIcon(R.drawable.sale)
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
-                .setContentText(message)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+        NotificationCompat.Builder notificationBuilder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = notifManager.getNotificationChannel(id);
+            if (mChannel == null) {
+                mChannel = new NotificationChannel(id, "promotionCode", importance);
+                mChannel.enableVibration(true);
+                notifManager.createNotificationChannel(mChannel);
+            }
+            notificationBuilder = new NotificationCompat.Builder(this, id)
+                    .setContentTitle("Promotion code used")
+                    .setSmallIcon(R.drawable.sale)
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                    .setContentText(message)
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent)
+                    .setChannelId(id);
+        } else {
+             notificationBuilder = new NotificationCompat.Builder(this, id)
+                    .setContentTitle("Promotion code used")
+                    .setSmallIcon(R.drawable.sale)
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                    .setContentText(message)
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent);
+        }
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notifManager.notify(NOTIFY_ID /* ID of notification */, notificationBuilder.build());
     }
 }
